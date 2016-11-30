d54e580eaba39926845e7563f801c5e5c9a602d7
Khlood Alluhaib <kka_237@hotmail.com>
2016-11-30 16:02:58 -0400

    adding basic checkout page- still empty

---
+++ checkout_page.php
@@ -0,0 +1,75 @@
+
+<html>
+<!-- Semantic UI CSS -->
+<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+
+<!-- Super Simple Slider CSS -->
+<link rel="stylesheet" type="text/css" href="sss/sss.css">
+<!-- Stylesheet Link -->
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
+<div class="ui grid">
+    <div class="computer tablet only row">
+        <div class="ui inverted fixed menu navbar page grid">
+            <a href="" class="brand item">Awesome Store</a>
+            <a href="index.php" class="active item"><i class="home icon"></i>Home</a>
+            <a class="ui dropdown item"><i class="shopping bag icon"></i>Products
+                <i class="dropdown icon"></i>
+                <div class="menu">
+                    <div class="item">Laptops</div>
+                    <div class="item">Televisions</div>
+                    <div class="item">Mobile Phones</div>
+                </div>
+            </a>
+            <div class="right menu">
+                <a href="#" class="item login"><i class="sign in icon"></i>Login</a>
+                <a href="#" class="item cart"><i class="shopping basket icon"></i>Cart</a>
+            </div>
+        </div>
+    </div>
+    <div class="mobile only row">
+        <div class="ui fixed inverted navbar menu">
+            <a href="" class="brand item">Awesome Store</a>
+            <div class="right menu open">
+                <a href="" class="menu item">
+                    <i class="content icon"></i>
+                </a>
+            </div>
+        </div>
+        <div class="ui vertical navbar menu">
+            <a href="" class="active item">Home</a>
+            <div class="ui item">
+                <div class="text">Products</div>
+                <div class="menu">
+                    <a class="item">Laptops</a>
+                    <a class="item">Televisions</a>
+                    <a class="item">Mobile Phones</a>
+                </div>
+            </div>
+            <div class="menu">
+                <a href="#" class="item login">Login</a>
+                <a href="#" class="item cart">Cart</a>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+<div> </div>
+
+<div class="ui center aligned segment">
+
+
+</div>
+<div class="ui two column stackable center aligned grid segment">
+  <div class="column">
+  </div>
+  <div class="column">
+  </div>
+</div>
+<!-- Footer -->
+<?php
+    include "include/footer.html"
+?>
+</div>
+</html>
