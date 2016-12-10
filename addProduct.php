648d8febca5f85248c5e29c870859f3cfda4ee36
alexander.sokolov <alexandersokolov17@gmail.com>
2016-12-09 23:01:36 -0400

    -
    -

---
+++ addProduct.php
@@ -0,0 +1,77 @@
+<html lang="en">
+<head>
+
+    <!-- Title -->
+    <title>Electronic Super Shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Semantic UI CSS -->
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+
+    <!-- Super Simple Slider CSS -->
+    <link rel="stylesheet" type="text/css" href="sss/sss.css">
+
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Web Centric Project">
+    <meta name="keywords" content="Web,Centric,Project">
+    <meta name="author" content="Team Squad">
+
+
+    <!-- Stylesheet Link -->
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
+    <!-- Product Link -->
+    <link rel="stylesheet" type="text/css" href="css/individualProduct.css">
+
+</head>
+
+
+<body>
+
+<!-- START OF NAVBAR MENU -->
+
+<?php include "include/navBar.php"; ?>
+
+<!-- END OF NAVBAR -->
+
+
+
+<!-- Login Modal -->
+<?php
+include "include/loginModal.html"
+?>
+
+<!-- Shopping Cart Modal -->
+<?php
+include "include/shoppingModal.html"
+?>
+
+<!-- jQuery -->
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+<!-- Semantic UI jQuery -->
+<script src="semantic/dist/semantic.min.js"></script>
+
+
+<div class="productContainer">
+    
+</div>
+
+
+
+
+
+<!-- Semantic UI Javascript -->
+<script src="js/semanticFuncionality.js"></script>
+</body>
+
+<footer>
+
+
+    <?php
+     //include "include/footer.html"
+    ?>
+
+</footer>
+</html>
